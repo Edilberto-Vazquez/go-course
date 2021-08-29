9c0a3c52959f1425aca08fc272e710c190d5bf27
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-08-29 00:41:27 -0500

    stringers

--- src/lessons/pointers-stringers.go
+++ src/lessons/pointers-stringers.go
@@ -20,6 +20,10 @@ func duplicateRAMFunc(myPC *pc) { // funcion paso por referencia
 	myPC.ram = myPC.ram * 2
 }
 
+func (myPc pc) String() string {
+	return fmt.Sprintf("%d ram, %d disk, %s brand", myPc.ram, myPc.disk, myPc.brand)
+}
+
 func Pointers() {
 	a := 50
 	b := &a // accede a la direccion de memoria
@@ -45,4 +49,5 @@ func Pointers() {
 	duplicateRAMFunc(&myPC)
 
 	fmt.Println(myPC)
+
 }
